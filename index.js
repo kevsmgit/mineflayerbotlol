b40ff524e5bfe03b2215016299da72e451045176
kevsmgit <kevsmilesmile@gmail.com>
2026-05-26 10:47:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const mineflayer = require('mineflayer')
 
+const bot = mineflayer.createBot({
+  host: 'your.server.ip',
+  port: 25565,
+  username: 'SpawnBot'
+})
+
+bot.on('spawn', () => {
+  console.log('Spawned')
+
+  setTimeout(() => {
+    bot.chat('/register Botiscool Botiscool') // replace with whatever
+    console.log('Sent join message')
+  }, 1000) // 1 second delay
+})
+
+bot.on('error', console.log)
+bot.on('end', () => console.log('Disconnected'))
